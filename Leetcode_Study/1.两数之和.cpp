0363001118203eecea3ce652d2152c38c634c2cc
Jason Chen <chen.hsinan2007@gmail.com>
2022-06-03 08:30:40 -0700

    update

---
+++ Leetcode_Study/1.两数之和.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+
+    // 2020.6.3, 极客时间覃超算法训练營, O(n^2)
+    vector<int> twoSum1(vector<int>& nums, int target) {
+        // answer vector to return
+        std::vector<int> answer;
+
+        for (int i = 0; i < nums.size(); i++) {
+            // one trick here is that j starts from (i + 1)
+            // so it won't be the same as i
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    // the returned answer needed the index, not its value
+                    answer.push_back(i);
+                    answer.push_back(j);
+                    return answer;
+                }
+            }
+        }
+
+        // no answer here
+        return answer;
+    }
+
+    // 2020.6.3, soulmachine, O(n)
+    // in the 2nd for loop, it only needs to find the corresponding index based on rest value
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // answer vector to return
+        std::vector<int> answer;
+
+        // build up lookup table, (key, value)
+        std::unordered_map<int, int> lookup;
+        for (int i = 0; i < nums.size(); i++) {
+            // key is number of nums[i], value is its index i
+            lookup.insert(std::make_pair(nums[i], i));
+        }
+
+        // iterate all elements, and find the rest from lookup table
+        for (int j = 0; j < nums.size(); j++) {
+            // rest is the corresponding (value, index)
+            int rest = target - nums[j];
+
+            // lookup now, so nums[j] is current value, rest is the corresponding value
+            // we only need to find the corresponding index from rest value
+            if ((lookup.find(rest) != lookup.end()) && (j != lookup[rest])) {
+                answer.push_back(j);
+                answer.push_back(lookup[rest]);
+                return answer;
+            }
+        }
+
+        // no answer here
+        return answer;
+    }
+
+};
+// @lc code=end
+
