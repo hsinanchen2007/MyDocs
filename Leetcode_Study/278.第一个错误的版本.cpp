7448d4b1efdc8134f601be8e653c4033481fbbe3
Jason Chen <chen.hsinan2007@gmail.com>
2022-06-20 06:49:47 -0700

    update

--- Leetcode_Study/278.第一个错误的版本.cpp
+++ Leetcode_Study/278.第一个错误的版本.cpp
@@ -46,7 +46,7 @@ class Solution {
 public:
 
     // 2022.6.19, by Hsinan. Use Binary Search
-    int firstBadVersion3(int n) {
+    int firstBadVersion4(int n) {
         // define left and right pointers, since the starting version is 1
         // so we use left = 1, and right is latest version n
         int left = 1, right = n;
@@ -63,7 +63,7 @@ public:
     }
 
     // 2022.6.19, from https://github.com/kamyu104/LeetCode-Solutions/blob/master/C++/single-element-in-a-sorted-array.cpp
-    int firstBadVersion2(int n) {
+    int firstBadVersion3(int n) {
         int left = 1, right = n;
         while (left <= right) {
             int mid = left + (right - left) / 2;
@@ -77,7 +77,7 @@ public:
     }
 
     // 2022.6.19, from https://www.guozet.me/leetcode/Leetcode-278-First-Bad-Version.html?h=first%20bad
-    int firstBadVersion1(int n) {
+    int firstBadVersion2(int n) {
         int left = 1, right = n;
         while (left < right) {
             int mid = left + (right - left) / 2;
@@ -87,7 +87,7 @@ public:
     }
 
     // 2022.6.19, from AcWing https://www.acwing.com/video/1653/
-    int firstBadVersion(int n) {
+    int firstBadVersion1(int n) {
         int left = 1, right = n;
         while (left < right) {
             int middle = left + (long long)right >> 1;
@@ -95,6 +95,51 @@ public:
             else left = middle + 1;
         }
         return right;
+    }
+
+    // 2022.6.19, from https://ke.algomooc.com/detail/v_6281aa6ce4b01a4851ff6237/3?from=p_6281a4bbe4b01a4851ff5f23&type=6&parent_pro_id=
+    int firstBadVersion(int n) {
+        // 注意到题目的第一个版本下标为 1 ，所以边界情况为 [ 1 , n ]
+        int left = 1 ;
+        int right = n ;
+
+        // 利用二分查找的方法，去定位出【第一个错误的版本】
+        // 在 while 循环里面，left 不断的 ++，而 right 不断的 --
+        // 当 left 和 right 相等， [ left , right ] 这个区间存在一个版本的时候
+        // 这个版本就是【第一个错误的版本】
+        // 所以，当 left == right 时，跳出循环
+        // 此时，while 循环的判断不可以使用等号
+        while (left < right) { 
+            // left + (right - left) / 2 和 (left + right) / 2 的结果相同
+            // 但是使用 left + (right - left) / 2 可以防止由于 left 、right 同时太大，导致相加的结果溢出的问题
+            // 比如数据 int 的最大值为 2,147,483,647
+            // 此时，left 为 2,147,483,640
+            // 此时，right 为 2,147,483,646
+            // 两者直接相加超过了 2,147,483,647，产生了溢出
+            int mid = left + (right - left) / 2;
+
+            // 调用系统函数，查看当前的版本是否是错误的版本
+            // 1、如果当前版本为错误的版本，那么此时 mid 指向的版本有可能是【第一个错误的版本】
+            // 因此，区间缩小为 [ left , mid ]
+            if (isBadVersion(mid)) {
+
+                // 再次注意，mid 指向的版本有可能是【第一个错误的版本】，因此，right 移动到 mid 的位置
+                right = mid;
+
+            // 2、如果当前版本为正确的版本，那么此时 mid 指向的版本绝对不可能是【第一个错误的版本】
+            //  因此，区间缩小为 [ mid + 1 , right ]
+            } else {
+                
+                // 再次注意，mid 指向的版本已经是正确的版本了
+                // 意味着 [ left , mid ] 的所有版本都是正确的，错误的版本发生在 [ mid + 1 , right ] 这个区间里面
+                left = mid + 1; 
+
+            }
+        }
+
+        // 当 left 和 right 相等， [ left , right ] 这个区间存在一个版本的时候
+        // 这个版本就是【第一个错误的版本】
+        return left;
     }
 
 };
--- Leetcode_Study/977.有序数组的平方.cpp
+++ Leetcode_Study/977.有序数组的平方.cpp
@@ -44,7 +44,7 @@ class Solution {
 public:
 
     // 2022.6.18, by Hsinan. Do sqrt then sort the answers by order
-    vector<int> sortedSquares3(vector<int>& nums) {
+    vector<int> sortedSquares4(vector<int>& nums) {
         for (int i = 0; i < nums.size(); i++) {
             long answer = nums[i] * nums[i];
             nums[i] = (int)answer;
@@ -57,7 +57,7 @@ public:
     // By using two pointers, as the given vector is already sorted, 
     // that means the max or min number will always be from either nums[0] or nums[size-1]
     // so define left and right pointers, and put the answers back to vector
-    vector<int> sortedSquares2(vector<int>& nums) {
+    vector<int> sortedSquares3(vector<int>& nums) {
         int k = nums.size() - 1;
         vector<int> answer(nums.size(), 0);
 
@@ -79,7 +79,7 @@ public:
     }
 
     // 2022.6.18, from https://github.com/kamyu104/LeetCode-Solutions/blob/master/C++/squares-of-a-sorted-array.cpp
-    vector<int> sortedSquares1(vector<int>& nums) {
+    vector<int> sortedSquares2(vector<int>& nums) {
         int right = distance(nums.cbegin(), lower_bound(nums.cbegin(), nums.cend(), 0));
         int left = right - 1;
         vector<int> result;
@@ -97,7 +97,7 @@ public:
     }
 
     // 2022.6.18, from AcWing https://www.acwing.com/video/3272/
-    vector<int> sortedSquares(vector<int>& nums) {
+    vector<int> sortedSquares1(vector<int>& nums) {
         int size = nums.size();
 
         // as below operations to answer is like an array operation,
@@ -115,6 +115,62 @@ public:
             }
         }
         return answer;
+    }
+
+    // 2022.6.19, from https://ke.algomooc.com/detail/v_626e7fcae4b01c509aaaf552/3?from=p_626e7eeee4b01c509aaaf51e&type=6&parent_pro_id=
+    // 登录 AlgoMooc 官网获取更多算法图解
+    // https://www.algomooc.com/582.html 
+    // 作者：程序员吴师兄
+    // 代码有看不懂的地方一定要私聊咨询吴师兄呀
+    // 有序数组的平方( LeetCode 977 ):https://leetcode-cn.com/problems/squares-of-a-sorted-array/
+    vector<int> sortedSquares(vector<int>& nums) {
+        // 我们设置两个索引，分别指向数组的两端
+
+        // right 指向终止位置
+        int right = nums.size() - 1;
+
+        // left 指向起始位置
+        int left = 0;
+
+        // 设置一个新数组 result 用来存放最终的输出结果
+        vector<int> result = vector<int>(nums.size(), 0);
+
+        // 让 index 指向 result 数组的终止位置，观察这个位置应该存放什么数字
+        int index = result.size() - 1;
+
+        // left 向右移动，right 向左移动，当 left 大于 right 时，说明已经观察遍历了 nums 数组中的所有元素，跳出循环
+        while (left <= right) {
+
+            // 说明左边的平方数大于右边
+            if (nums[left] * nums[left] > nums[right] * nums[right]) {
+
+                // result 数组中 index 位置要存放更大的那个数，即 nums[left] * nums[left]
+                result[index] = nums[left] * nums[left];
+
+                // 由于相对较大的数是在 left 位置，上一行代码已经将它赋值到 index 位置
+                // 所以此时 left 位置的数已经失去作用，将它向后移动
+                left++;
+
+                // 此时，index 位置已经存放好数，将它向前移动，观察下一个位置应该存放哪个数
+                index--;
+
+              // 说明右边的平方数大于左边
+            } else {
+
+                // result 数组中 index 位置要存放更大的那个数，即 nums[right] * nums[right]
+                result[index] = nums[right] * nums[right];
+
+                // 由于相对较大的数是在 right 位置，上一行代码已经将它赋值到 index 位置
+                // 所以此时 right 位置的数已经失去作用，将它向前移动
+                right--;
+
+                // 此时，index 位置已经存放好数，将它向前移动，观察下一个位置应该存放哪个数
+                index--;
+            }
+        }
+
+        // 最后返回我们设置的结果数组即可
+        return result;
     }
 
 };
