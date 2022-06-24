30948a43999e2a121bbc3dec53f5bd20bf80358e
Jason Chen <chen.hsinan2007@gmail.com>
2022-06-24 11:59:54 -0700

    update

---
+++ AcWing/算法基础课/794. 高精度除法.cpp
@@ -0,0 +1,69 @@
+/*
+
+作者：yxc
+链接：https://www.acwing.com/activity/content/code/content/39795/
+来源：AcWing
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+794. 高精度除法
+
+给定两个非负整数（不含前导 0） A，B，请你计算 A/B 的商和余数。
+
+输入格式
+共两行，第一行包含整数 A，第二行包含整数 B。
+
+输出格式
+共两行，第一行输出所求的商，第二行输出所求余数。
+
+数据范围
+1≤A的长度≤100000,
+1≤B≤10000,
+B 一定不为 0
+输入样例：
+7
+2
+输出样例：
+3
+1
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> div(vector<int> &A, int b, int &r)
+{
+    vector<int> C;
+    r = 0;
+    for (int i = A.size() - 1; i >= 0; i -- )
+    {
+        r = r * 10 + A[i];
+        C.push_back(r / b);
+        r %= b;
+    }
+    reverse(C.begin(), C.end());
+    while (C.size() > 1 && C.back() == 0) C.pop_back();
+    return C;
+}
+
+int main()
+{
+    string a;
+    vector<int> A;
+
+    int B;
+    cin >> a >> B;
+    for (int i = a.size() - 1; i >= 0; i -- ) A.push_back(a[i] - '0');
+
+    int r;
+    auto C = div(A, B, r);
+
+    for (int i = C.size() - 1; i >= 0; i -- ) cout << C[i];
+
+    cout << endl << r << endl;
+
+    return 0;
+}
