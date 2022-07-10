6d29b6800ca9889d916d217f34f66a00bec19547
Jason Chen <chen.hsinan2007@gmail.com>
2022-07-10 11:59:13 -0700

    update

--- AlgoMonster/Binary Search/Binary Search.cpp
+++ AlgoMonster/Binary Search/Binary Search.cpp
@@ -35,6 +35,30 @@ each other as we search. When moving the index, we discard elements and shrink t
 
 Time Complexity: O(log(n))
 
+Calculating mid
+Note that when calculating mid, if the number of elements is even, there are two elements in the middle. We usually follow the convention of picking the first one, equivalent to doing integer division (left + right) / 2.
+
+In most programming languages, we calculate mid with left + floor((right-left) / 2) to avoid potential integer overflow. However, in Python, we do not need to worry about left + right integer overflow because Python3 integers can be arbitrarily large.
+
+Deducing binary search
+It's essential to understand and deduce the algorithm yourself instead of memorizing it. In an actual interview, interviewers may ask you additional questions to test your understanding, so simply memorizing the algorithm may not be enough to convince the interviewer.
+
+Key elements in writing a correct binary search:
+
+1. When to terminate the loop
+Make sure the while loop has an equality comparison. Otherwise, we'd skip the loop and miss the potential match for the edge case of a one-element array.
+
+2. Whether/how to update left and right boundary in the if conditions
+Consider which side to discard. If arr[mid] is already smaller than the target, we should discard everything on the left by making left = mid + 1.
+
+3. Should I discard the current element?
+For vanilla binary search, we can discard it since it can't be the final answer if it is not equal to the target. There might be situations where you would want to think twice before discarding the current element. We'll discuss this in the next module.
+
+When to use binary search
+Interestingly, binary search works beyond sorted arrays. You can use binary search whenever you make a binary decision to shrink the search range. We will see this in the following modules.
+
+P.S. Did you notice AlgoMonster's logo is an illustration of binary search? :P
+
 */
 
 int binary_search(std::vector<int> arr, int target) {
