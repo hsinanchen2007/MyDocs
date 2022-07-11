fc31104e4b94b399e7045bdc85ad5490fff01c61
Jason Chen <chen.hsinan2007@gmail.com>
2022-07-10 21:30:52 -0700

    update

--- Leetcode_Study/74.搜索二维矩阵.cpp
+++ Leetcode_Study/74.搜索二维矩阵.cpp
@@ -140,8 +140,24 @@ public:
         return false;
     }
 
+    // 2022.7.10
+    // 作者：yxc
+    // 链接：https://www.acwing.com/activity/content/code/content/370359/
+    // 来源：AcWing
+    // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty()) return false;
+        int n = matrix.size(), m = matrix[0].size();
 
+        int l = 0, r = n * m - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (matrix[mid / m][mid % m] >= target) r = mid;
+            else l = mid + 1;
+        }
 
+        return matrix[r / m][r % m] == target;
+    }
     
 };
 // @lc code=end
