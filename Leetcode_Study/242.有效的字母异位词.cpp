332fe080d5bb67e47860b394622ea9a29f157895
Jason Chen <chen.hsinan2007@gmail.com>
2022-07-27 16:44:05 -0700

    update

--- Leetcode_Study/242.有效的字母异位词.cpp
+++ Leetcode_Study/242.有效的字母异位词.cpp
@@ -148,6 +148,36 @@ public:
 class Solution97 {
 public:
     // 2022.7.23, from https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0242.%E6%9C%89%E6%95%88%E7%9A%84%E5%AD%97%E6%AF%8D%E5%BC%82%E4%BD%8D%E8%AF%8D.md
+    /*
+        本题B站视频讲解版：学透哈希表，数组使用有技巧！Leetcode：242.有效的字母异位词
+
+        先看暴力的解法，两层for循环，同时还要记录字符是否重复出现，很明显时间复杂度是 O(n^2)。
+        暴力的方法这里就不做介绍了，直接看一下有没有更优的方式。
+
+        数组其实就是一个简单哈希表，而且这道题目中字符串只有小写字符，那么就可以定义一个数组，来记录字符串s里字符出现的次数。
+        如果对哈希表的理论基础关于数组，set，map不了解的话可以看这篇：关于哈希表，你该了解这些！
+
+        需要定义一个多大的数组呢，定一个数组叫做record，大小为26 就可以了，初始化为0，因为字符a到字符z的ASCII也是26个连续的数值。
+        为了方便举例，判断一下字符串s= "aee", t = "eae"。
+
+        操作动画如下：
+
+        242.有效的字母异位词
+
+        定义一个数组叫做record用来上记录字符串s里字符出现的次数。
+
+        需要把字符映射到数组也就是哈希表的索引下标上，因为字符a到字符z的ASCII是26个连续的数值，所以字符a映射为下标0，相应的字符z映射为下标25。
+        再遍历 字符串s的时候，只需要将 s[i] - ‘a’ 所在的元素做+1 操作即可，并不需要记住字符a的ASCII，只要求出一个相对数值就可以了。 这样就将字符串
+        s中字符出现的次数，统计出来了。
+
+        那看一下如何检查字符串t中是否出现了这些字符，同样在遍历字符串t的时候，对t中出现的字符映射哈希表索引上的数值再做-1的操作。
+        那么最后检查一下，record数组如果有的元素不为零0，说明字符串s和t一定是谁多了字符或者谁少了字符，return false。
+
+        最后如果record数组所有元素都为零0，说明字符串s和t是字母异位词，return true。
+        时间复杂度为O(n)，空间上因为定义是的一个常量大小的辅助数组，所以空间复杂度为O(1)。
+
+        C++ 代码如下：
+    */
     bool isAnagram(string s, string t) {
         int record[26] = {0};
         for (int i = 0; i < s.size(); i++) {
@@ -172,6 +202,8 @@ public:
 class Solution96 {
 public:
     // 2022.7.23, from https://github.com/kamyu104/LeetCode-Solutions/blob/master/C++/valid-anagram.cpp
+    // Time:  O(n)
+    // Space: O(1)
     bool isAnagram(string s, string t) {
         if (size(s) != size(t)) {
             return false;
@@ -195,6 +227,8 @@ public:
 class Solution95 {
 public:
     // 2022.7.23, from https://github.com/kamyu104/LeetCode-Solutions/blob/master/C++/valid-anagram.cpp
+    // Time:  O(nlogn)
+    // Space: O(1)
     bool isAnagram(string s, string t) {
         if (size(s) != size(t)) {
             return false;
@@ -207,7 +241,7 @@ public:
 };
 
 
-class Solution {
+class Solution94 {
 public:
     // 2022.7.23, from AcWing https://www.acwing.com/activity/content/code/content/445280/
     // use C++ STL's build-in "==" to check the difference
@@ -218,5 +252,25 @@ public:
         return a == b;
     }
 };
+
+
+// OJ: https://leetcode.com/problems/valid-anagram/
+// Author: github.com/lzl124631x
+// Time: O(N)
+// Space: O(C)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int cnt[26] = {};
+        for (char c : s) cnt[c - 'a']++;
+        for (char c : t) cnt[c - 'a']--;
+        for (int n : cnt) {
+            if (n) return false;
+        }
+        return true;
+    }
+};
+
+
 // @lc code=end
 
