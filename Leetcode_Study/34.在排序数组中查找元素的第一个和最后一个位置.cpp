dd95a901c861abd77599591a212ef0f3090d6469
Jason Chen <chen.hsinan2007@gmail.com>
2022-07-27 22:17:08 -0700

    update

--- Leetcode_Study/34.在排序数组中查找元素的第一个和最后一个位置.cpp
+++ Leetcode_Study/34.在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -15,7 +15,6 @@ https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-a
 
 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
 
- 
 
 示例 1：
 
@@ -692,7 +691,7 @@ public:
 };
 
 
-class Solution {
+class Solution94 {
 public:
     // 2022.7.24, from https://walkccc.me/LeetCode/problems/0034/
     // Time: O(log n)
@@ -705,5 +704,98 @@ public:
         return {l, r};
     }
 };
+
+
+class Solution93 {
+public:
+    // 2022.7.27, from https://github.com/grandyang/leetcode/issues/34
+    /*
+        这道题让我们在一个有序整数数组中寻找相同目标值的起始和结束位置，而且限定了时间复杂度为 O(logn)，这是典型的二分查找法的时间复杂度，
+        所以这里也需要用此方法，思路是首先对原数组使用二分查找法，找出其中一个目标值的位置，然后向两边搜索找出起始和结束的位置，代码如下：
+
+        解法一:
+    */
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int idx = search(nums, 0, nums.size() - 1, target);
+        if (idx == -1) return {-1, -1};
+        int left = idx, right = idx;
+        while (left > 0 && nums[left - 1] == nums[idx]) --left;
+        while (right < nums.size() - 1 && nums[right + 1] == nums[idx]) ++right;
+        return {left, right};
+    }
+    int search(vector<int>& nums, int left, int right, int target) {
+        if (left > right) return -1;
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) return mid;
+        if (nums[mid] < target) return search(nums, mid + 1, right, target);
+        else return search(nums, left, mid - 1, target);
+    }
+};
+
+
+class Solution92 {
+public:
+    // 2022.7.27, from https://github.com/grandyang/leetcode/issues/34
+    /*
+        可能有些人会觉得上面的算法不是严格意义上的 O(logn) 的算法，因为在最坏的情况下会变成 O(n)，比如当数组里的数全是目标值的话，
+        从中间向两边找边界就会一直遍历完整个数组，那么下面来看一种真正意义上的 O(logn) 的算法，使用两次二分查找法，第一次找到左边界，
+        第二次调用找到右边界即可，具体代码如下：
+
+        解法二：
+    */
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res(2, -1);
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid;
+        }
+        if (right == nums.size() || nums[right] != target) return res;
+        res[0] = right;
+        right = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) left = mid + 1;
+            else right = mid;
+        }
+        res[1] = right - 1;
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    // 2022.7.27, from https://github.com/grandyang/leetcode/issues/34
+    /*
+        其实我们也可以只使用一个二分查找的子函数，来同时查找出第一个和最后一个位置。如何只用查找第一个大于等于目标值的二分函数来查找整个范围呢，
+        这里用到了一个小 trick，首先来查找起始位置的 target，就是在数组中查找第一个大于等于 target 的位置，当返回的位置越界，或者该位置上的
+        值不等于 target 时，表示数组中没有 target，直接返回 {-1, -1} 即可。若查找到了 target 值，则再查找第一个大于等于 target+1 的位置，
+        然后把返回的位置减1，就是 target 的最后一个位置，即便是返回的值越界了，减1后也不会越界，这样就实现了使用一个二分查找函数来解题啦，参见代码如下：
+
+        解法三：
+    */
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start = firstGreaterEqual(nums, target);
+        if (start == nums.size() || nums[start] != target) return {-1, -1};
+        return {start, firstGreaterEqual(nums, target + 1) - 1};
+    }
+    int firstGreaterEqual(vector<int>& nums, int target) {
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid;
+        }
+        return right;
+    }
+};
+
+
+/************************************************************************************************************/
+/************************************************************************************************************/
+
+
 // @lc code=end
 
--- Leetcode_Study/35.搜索插入位置.cpp
+++ Leetcode_Study/35.搜索插入位置.cpp
@@ -297,7 +297,7 @@ public:
 };
 
 
-class Solution {
+class Solution94 {
 public:
     // 2022.7.24, from https://walkccc.me/LeetCode/problems/0035/
     // Time: O(log n)
@@ -319,5 +319,94 @@ public:
         return l;
     }
 };
+
+
+class Solution93 {
+public:
+    // 2022.7.27, from https://github.com/grandyang/leetcode/issues/35
+    /*
+        这道题基本没有什么难度，实在不理解为啥还是 Medium 难度的，完完全全的应该是 Easy 啊（貌似现在已经改为 Easy 类了），三行代码搞定的题，
+        只需要遍历一遍原数组，若当前数字大于或等于目标值，则返回当前坐标，如果遍历结束了，说明目标值比数组中任何一个数都要大，则返回数组长度n即可，代码如下：
+
+        解法一：
+    */
+    int searchInsert(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] >= target) return i;
+        }
+        return nums.size();
+    }
+};
+
+
+class Solution92 {
+public:
+    // 2022.7.27, from https://github.com/grandyang/leetcode/issues/35
+    /*
+        当然，我们还可以用二分搜索法来优化时间复杂度，而且个人认为这种方法应该是面试官们想要考察的算法吧，属于博主之前的总结帖 LeetCode Binary Search 
+        Summary 二分搜索法小结 中第二类 - 查找不小于目标值的数，参见代码如下：
+
+        解法二：
+    */
+    int searchInsert(vector<int>& nums, int target) {
+        if (nums.back() < target) return nums.size();
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid;
+        }
+        return right;
+    }
+};
+
+
+
+class Solution91 {
+public:
+    // 2022.7.27, from https://github.com/lzl124631x/LeetCode/tree/master/leetcode/35.%20Search%20Insert%20Position
+    // OJ: https://leetcode.com/problems/search-insert-position/
+    // Author: github.com/lzl124631x
+    // Time: O(logN)
+    // Space: O(1)
+    // Solution 1. Binary Search (L <= R)
+    int searchInsert(vector<int>& A, int target) {
+        int L = 0, R = A.size() - 1;
+        while (L <= R) {
+            int M = (L + R) / 2;
+            if (A[M] == target) return M;
+            if (A[M] < target) L = M + 1;
+            else R = M - 1;
+        }
+        return L;
+    }
+};
+
+
+
+class Solution {
+public:
+    // 2022.7.27, from https://github.com/lzl124631x/LeetCode/tree/master/leetcode/35.%20Search%20Insert%20Position
+    // OJ: https://leetcode.com/problems/search-insert-position/
+    // Author: github.com/lzl124631x
+    // Time: O(logN)
+    // Space: O(1)
+    // Solution 2. Binary Search (L < R)
+    int searchInsert(vector<int>& A, int target) {
+        int L = 0, R = A.size();
+        while (L < R) {
+            int M = (L + R) / 2;
+            if (A[M] < target) L = M + 1;
+            else R = M;
+        }
+        return L;
+    }
+};
+
+
+/************************************************************************************************************/
+/************************************************************************************************************/
+
+
 // @lc code=end
 
