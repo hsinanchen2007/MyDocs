74a4093e959b2a2405e7b490f8ae368288889044
Jason Chen <chen.hsinan2007@gmail.com>
2022-08-11 13:55:59 -0700

    update

--- educative/Grokking The Coding Interviews Patterns For Coding Questions/Slidding Window/Longest Substring with Distinct Characters (hard) v1.cpp
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/Slidding Window/Longest Substring with Distinct Characters (hard) v1.cpp
@@ -23,3 +23,36 @@ Output: 3
 Explanation: Longest substrings with distinct characters are "abc" & "cde".
 
 */
+
+using namespace std;
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class NoRepeatSubstring {
+ public:
+  static int findLength(const string& str) {
+    int maxLength = 0;
+    // TODO: Write your code here
+    int start = 0;
+    unordered_map<char, int> hashTbl;
+    for (int end = 0; end < str.size(); end++) {
+      hashTbl[str[end]]++;
+      for (auto item : hashTbl) {
+        if (item.second > 1) {
+          hashTbl[item.first]--;
+          if (hashTbl[item.first] == 0) {
+            hashTbl.erase(item.first);
+          }
+          start = end;
+          break;
+        }
+      }
+      
+      maxLength = max(maxLength, end - start + 1);
+    }
+    return maxLength;
+  }
+};
+
