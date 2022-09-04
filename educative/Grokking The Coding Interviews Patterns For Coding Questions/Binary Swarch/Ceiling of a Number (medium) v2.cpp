8157f5292fad2725c1265ab89e716b03fa35b5b9
Jason Chen <chen.hsinan2007@gmail.com>
2022-09-04 08:59:22 -0700

    update

---
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/Binary Swarch/Ceiling of a Number (medium) v2.cpp
@@ -0,0 +1,82 @@
+/*
+
+https://www.educative.io/courses/grokking-the-coding-interview/qA5wW7R8ox7
+
+Problem Statement#
+Given an array of numbers sorted in an ascending order, find the ceiling of a given number 
+‘key’. The ceiling of the ‘key’ will be the smallest element in the given array greater than 
+or equal to the ‘key’.
+
+Write a function to return the index of the ceiling of the ‘key’. If there isn’t any ceiling 
+return -1.
+
+Example 1:
+
+Input: [4, 6, 10], key = 6
+Output: 1
+Explanation: The smallest number greater than or equal to '6' is '6' having index '1'.
+
+Example 2:
+
+Input: [1, 3, 8, 10, 15], key = 12
+Output: 4
+Explanation: The smallest number greater than or equal to '12' is '15' having index '4'.
+
+Example 3:
+
+Input: [4, 6, 10], key = 17
+Output: -1
+Explanation: There is no number greater than or equal to '17' in the given array.
+
+Example 4:
+
+Input: [4, 6, 10], key = -1
+Output: 0
+Explanation: The smallest number greater than or equal to '-1' is '4' having index '0'.
+
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class CeilingOfANumber {
+ public:
+  static int searchCeilingOfANumber(const vector<int>& arr, int key) {
+    /*
+    // TODO: Write your code here
+    auto ret = std::lower_bound(arr.begin(), arr.end(), key);
+    if (ret != arr.end()) {
+      return std::distance(arr.begin(), ret);
+    }
+    */
+
+    int left = 0;
+    int right = arr.size() - 1;
+    while (left <= right) {
+      int middle = left + (right - left) / 2;
+      if (arr[middle] < key) {
+        left = middle + 1;
+      } else if (arr[middle] > key) {
+        right = middle - 1;
+      } else if (arr[middle] == key) {
+        left = middle;
+        break;
+      }
+    }
+
+    // sanity check for out of bound
+    if (left == arr.size()) return -1;
+
+    return left;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  cout << CeilingOfANumber::searchCeilingOfANumber(vector<int>{4, 6, 6}, 6) << endl;
+  cout << CeilingOfANumber::searchCeilingOfANumber(vector<int>{1, 3, 8, 10, 15}, 12) << endl;
+  cout << CeilingOfANumber::searchCeilingOfANumber(vector<int>{4, 6, 10}, 17) << endl;
+  cout << CeilingOfANumber::searchCeilingOfANumber(vector<int>{4, 6, 10}, -1) << endl;
+}
+
