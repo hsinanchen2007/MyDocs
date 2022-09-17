8a0ae8f5901a644e4551f5146e433be3402b6bca
Jason Chen <chen.hsinan2007@gmail.com>
2022-09-16 19:35:00 -0700

    update

---
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/Two Pointers/Dutch Nationa Flag Problem (medium) v1.cpp
@@ -0,0 +1,57 @@
+/*
+
+https://www.educative.io/courses/grokking-the-coding-interview/RMBxV6jz6Q0
+
+Problem Statement
+Given an array containing 0s, 1s and 2s, sort the array in-place. You should treat numbers of the array 
+as objects, hence, we can’t count 0s, 1s, and 2s to recreate the array.
+
+The flag of the Netherlands consists of three colors: red, white and blue; and since our input array also 
+consists of three different numbers that is why it is called Dutch National Flag problem.
+
+Example 1:
+
+Input: [1, 0, 2, 1, 0]
+Output: [0, 0, 1, 1, 2]
+
+Example 2:
+
+Input: [2, 2, 0, 1, 2, 0]
+Output: [0, 0, 1, 2, 2, 2,]
+
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class DutchFlag {
+ public:
+  static void sort(vector<int> &arr) {
+    // TODO: Write your code here   
+    // all elements < low are 0 and all elements > high are 2
+    // all elements from >= low < i are 1
+    int low = 0, high = arr.size() - 1;
+    for (int i = 0; i <= high;) {
+      if (arr[i] == 0) {
+        swap(arr, i, low);
+        i++;
+        low++;
+      } else if (arr[i] == 1) {
+        i++;
+      } else {  // the case for arr[i] == 2
+        swap(arr, i, high);
+        // decrement 'high' only, after the swap the number at index 'i' could be 0, 1 or 2
+        high--;
+      }
+    }
+  }
+
+ private:
+  static void swap(vector<int> &arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+};
