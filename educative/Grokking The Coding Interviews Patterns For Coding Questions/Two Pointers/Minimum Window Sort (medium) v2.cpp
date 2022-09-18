3965fb47a1d0bd3db82f545e4fa95745bdb70102
Jason Chen <chen.hsinan2007@gmail.com>
2022-09-17 17:44:15 -0700

    update

---
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/Two Pointers/Minimum Window Sort (medium) v2.cpp
@@ -0,0 +1,97 @@
+/*
+
+Solution#
+As we know, once an array is sorted (in ascending order), the smallest number is at the beginning and 
+the largest number is at the end of the array. So if we start from the beginning of the array to find 
+the first element which is out of sorting order i.e., which is smaller than its previous element, and 
+similarly from the end of array to find the first element which is bigger than its previous element, 
+will sorting the subarray between these two numbers result in the whole array being sorted?
+
+Let’s try to understand this with Example-2 mentioned above. In the following array, what are the first 
+numbers out of sorting order from the beginning and the end of the array:
+
+    [1, 3, 2, 0, -1, 7, 10]
+Starting from the beginning of the array the first number out of the sorting order is ‘2’ as it is 
+smaller than its previous element which is ‘3’.
+
+Starting from the end of the array the first number out of the sorting order is ‘0’ as it is bigger 
+than its previous element which is ‘-1’
+
+As you can see, sorting the numbers between ‘3’ and ‘-1’ will not sort the whole array. To see this, 
+the following will be our original array after the sorted subarray:
+
+    [1, -1, 0, 2, 3, 7, 10]
+The problem here is that the smallest number of our subarray is ‘-1’ which dictates that we need to 
+include more numbers from the beginning of the array to make the whole array sorted. We will have a 
+similar problem if the maximum of the subarray is bigger than some elements at the end of the array. 
+To sort the whole array we need to include all such elements that are smaller than the biggest element 
+of the subarray. So our final algorithm will look like:
+
+From the beginning and end of the array, find the first elements that are out of the sorting order. 
+The two elements will be our candidate subarray.
+
+Find the maximum and minimum of this subarray.
+
+Extend the subarray from beginning to include any number which is bigger than the minimum of the subarray.
+
+Similarly, extend the subarray from the end to include any number which is smaller than the maximum of the subarray.
+
+Time complexity#
+The time complexity of the above algorithm will be O(N).
+
+Space complexity#
+The algorithm runs in constant space O(1).
+
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <limits>
+#include <vector>
+
+class ShortestWindowSort {
+ public:
+  static int sort(const vector<int>& arr) {
+    int low = 0, high = arr.size() - 1;
+    // find the first number out of sorting order from the beginning
+    while (low < arr.size() - 1 && arr[low] <= arr[low + 1]) {
+      low++;
+    }
+
+    if (low == arr.size() - 1) {  // if the array is sorted
+      return 0;
+    }
+
+    // find the first number out of sorting order from the end
+    while (high > 0 && arr[high] >= arr[high - 1]) {
+      high--;
+    }
+
+    // find the maximum and minimum of the subarray
+    int subarrayMax = numeric_limits<int>::min(), subarrayMin = numeric_limits<int>::max();
+    for (int k = low; k <= high; k++) {
+      subarrayMax = max(subarrayMax, arr[k]);
+      subarrayMin = min(subarrayMin, arr[k]);
+    }
+
+    // extend the subarray to include any number which is bigger than the minimum of the subarray
+    while (low > 0 && arr[low - 1] > subarrayMin) {
+      low--;
+    }
+    // extend the subarray to include any number which is smaller than the maximum of the subarray
+    while (high < arr.size() - 1 && arr[high + 1] < subarrayMax) {
+      high++;
+    }
+
+    return high - low + 1;
+  }
+};
+
+int main(int argc, char* argv[]) {
+  cout << ShortestWindowSort::sort(vector<int>{1, 2, 5, 3, 7, 10, 9, 12}) << endl;
+  cout << ShortestWindowSort::sort(vector<int>{1, 3, 2, 0, -1, 7, 10}) << endl;
+  cout << ShortestWindowSort::sort(vector<int>{1, 2, 3}) << endl;
+  cout << ShortestWindowSort::sort(vector<int>{3, 2, 1}) << endl;
+}
+
