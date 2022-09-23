fd33d7206cc1a14a6f5502d9ddcf92672128c710
Hsinan Chen <chen.hsinan2007@gmail.com>
2022-09-22 17:14:33 -0700

    update

--- C++/STL/algorithm/Minimum-maximum-operations/max_element_min_element.cc
+++ C++/STL/algorithm/Minimum-maximum-operations/max_element_min_element.cc
@@ -34,6 +34,19 @@ Return value
 Iterator to the greatest element in the range [first, last). If several elements in the range are equivalent 
 to the greatest element, returns the iterator to the first such element. Returns last if the range is empty.
 
+
+Finds the smallest element in the range [first, last).
+
+1) Elements are compared using operator<.
+3) Elements are compared using the given binary comparison function comp.
+2,4) Same as (1,3), but executed according to policy. These overloads do not participate in overload 
+resolution unless std::is_execution_policy_v<std::decay_t<ExecutionPolicy>> (until C++20) 
+std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>> (since C++20) is true.
+
+Return value
+Iterator to the smallest element in the range [first, last). If several elements in the range are equivalent 
+to the smallest element, returns the iterator to the first such element. Returns last if the range is empty.
+
 */
 
 static bool abs_compare(int a, int b)
@@ -54,7 +67,17 @@ int main()
     result = std::max_element(v.begin(), v.end(), abs_compare);
     std::cout << "max element (absolute) at index : " << std::distance(v.begin(), result) << " value " << *result << '\n';
 
+    // regular min_element which check the original values
+    result = std::min_element(v.begin(), v.end());
+    std::cout << "min element at index : " << std::distance(v.begin(), result) << " value " << *result << '\n';
+ 
+    // use abs_compare() to check absolute values
+    result = std::min_element(v.begin(), v.end(), abs_compare);
+    std::cout << "min element (absolute) at index : " << std::distance(v.begin(), result) << " value " << *result << '\n';
+
     return 0;
     // max element at index : 5 value 9
     // max element (absolute) at index : 2 value -14
+    // min element at index : 2 value -14
+    // min element (absolute) at index : 1 value 1
 }
--- C++/STL/algorithm/Minimum-maximum-operations/max_min.cc
+++ C++/STL/algorithm/Minimum-maximum-operations/max_min.cc
@@ -34,6 +34,19 @@ Return value
 3-4) The greatest value in ilist. If several values are equivalent to the greatest, returns 
 the leftmost one.
 
+
+Returns the smaller of the given values.
+
+1-2) Returns the smaller of a and b.
+3-4) Returns the smallest of the values in initializer list ilist.
+The (1,3) versions use operator< to compare the values, the (2,4) versions use the given 
+comparison function comp.
+
+Return value
+1-2) The smaller of a and b. If the values are equivalent, returns a.
+3-4) The smallest value in ilist. If several values are equivalent to the smallest, returns 
+the leftmost such value.
+
 */
 
 int main()
@@ -53,10 +66,14 @@ int main()
     // cannot do this way, the elements needed to be initialized and called inside std::max()
     // cout << std::max(myF) << endl;
 
+    // return min element
+    cout << std::min({-0.51, 1.38, 4.96, 2.87, 3.13, 0.95, 8.96}) << endl;
+
     return 0;
     // larger of 1 and 9999 is 9999
     // larger of 'a', and 'b' is 'b'
     // largest of 1, 10, 50, and 200 is 200
     // longest of "foo", "bar", and "hello" is "hello"
     // 8.96
+    // -0.51
 }
