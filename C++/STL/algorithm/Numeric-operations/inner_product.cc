ac3f78fc108eb2a68d4d1a99b089ce226c544ea2
Jason Chen <chen.hsinan2007@gmail.com>
2022-09-24 20:45:13 -0700

    update

--- C++/STL/algorithm/Numeric-operations/inner_product.cc
+++ C++/STL/algorithm/Numeric-operations/inner_product.cc
@@ -60,9 +60,11 @@ int main()
     std::vector<int> a{0, 1, 2, 3, 4};
     std::vector<int> b{5, 4, 2, 3, 1};
  
+    // 0*5 + 1*4 + 2*2 + 3*3 + 4*1 = 21
     int r1 = std::inner_product(a.begin(), a.end(), b.begin(), 0);
     std::cout << "Inner product of a and b: " << r1 << '\n';
  
+    // TODO: how to calculate below?
     int r2 = std::inner_product(a.begin(), a.end(), b.begin(), 0,
                                 std::plus<>(), std::equal_to<>());
     std::cout << "Number of pairwise matches between a and b: " <<  r2 << '\n';
