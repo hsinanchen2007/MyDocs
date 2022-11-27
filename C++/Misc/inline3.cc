bf83def1289b86ab1a7c39138564cf34090bf266
Hsinan Chen <chen.hsinan2007@gmail.com>
2022-11-26 19:47:29 -0800

    update

--- C++/Misc/inline3.cc
+++ C++/Misc/inline3.cc
@@ -21,6 +21,31 @@
     这种在函数调用处直接嵌入函数体的函数称为内联函数（Inline Function），又称内嵌函数或者内置函数。
 
     指定内联函数的方法很简单，只需要在函数定义处增加 inline 关键字。请看下面的例子：
+
+    注意，要在函数定义处添加 inline 关键字，在函数声明处添加 inline 关键字虽然没有错，但这种做法是无效的，编译器会忽略函数
+    声明处的 inline 关键字。
+
+    当编译器遇到函数调用swap(&m, &n)时，会用 swap() 函数的代码替换swap(&m, &n)，同时用实参代替形参。这样，程序第 16 
+    行就被置换成：
+
+        int temp;
+        temp = *(&m);
+        *(&m) = *(&n);
+        *(&n) = temp;
+
+    编译器可能会将 *(&m)、*(&n) 分别优化为 m、n。
+
+    当函数比较复杂时，函数调用的时空开销可以忽略，大部分的 CPU 时间都会花费在执行函数体代码上，所以我们一般是将非常短小的函数
+    声明为内联函数。
+
+    使用内联函数的缺点也是非常明显的，编译后的程序会存在多份相同的函数拷贝，如果被声明为内联函数的函数体非常大，那么编译后的程序
+    体积也将会变得很大，所以再次强调，一般只将那些短小的、频繁调用的函数声明为内联函数。
+
+    内联函数看起来简单，但是有很多细节要引起重视，我们将在后续两节《C++内联函数也可以用来代替宏》《如何规范地使用C++内联函数》
+    对内联函数的本质以及使用规范进行深入的讲解。
+
+    最后需要说明的是，对函数作 inline 声明只是程序员对编译器提出的一个建议，而不是强制性的，并非一经指定为 inline 编译器就
+    必须这样做。编译器有自己的判断能力，它会根据具体情况决定是否这样做。
 */
 
 #include <iostream>
@@ -40,7 +65,11 @@ int main(){
     cin>>m>>n;
 
     cout<<m<<", "<<n<<endl;
+
+    // compiler will expand the real code in swap() here so it saves unnecessary runtime between main and 
+    // other functions, so once it is compiled, the main function will have real code in swap to main
     swap(&m, &n);
+
     cout<<m<<", "<<n<<endl;
 
     return 0;
