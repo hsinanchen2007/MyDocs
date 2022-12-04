568f0b72cf5d035aa321aa4bb2412bb4d5a93962
Hsinan Chen <chen.hsinan2007@gmail.com>
2022-12-04 10:00:23 -0800

    update

--- educative/Grokking The Coding Interviews Patterns For Coding Questions/Fast Slow Pointers/LinkedList Cycle (easy) v2.cpp
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/Fast Slow Pointers/LinkedList Cycle (easy) v2.cpp
@@ -1,35 +1,6 @@
 /*
 
-Introduction
-
-The Fast & Slow pointer approach, also known as the Hare & Tortoise algorithm, is a pointer algorithm 
-that uses two pointers which move through the array (or sequence/LinkedList) at different speeds. 
-This approach is quite useful when dealing with cyclic LinkedLists or arrays.
-
-By moving at different speeds (say, in a cyclic LinkedList), the algorithm proves that the two pointers 
-are bound to meet. The fast pointer should catch the slow pointer once both the pointers are in a cyclic 
-loop.
-
-One of the famous problems solved using this technique was Finding a cycle in a LinkedList. Let’s jump 
-onto this problem to understand the Fast & Slow pattern.
-
-
 https://www.educative.io/courses/grokking-the-coding-interview/N7rwVyAZl6D
-
-Problem Statement#
-Given the head of a Singly LinkedList, write a function to determine if the LinkedList has a cycle in it or not.
-
- Example:  
- head  
- Following LinkedList has a cycle:  
- Following LinkedList doesn't have a cycle:  
-    2   
-    4   
-    6   
-    8   
-    10   
-    null   
-
 
 Solution
 Imagine two racers running in a circular racing track. If one racer is faster than the other, the faster racer 
