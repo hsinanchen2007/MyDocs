0a74b2ea5b8bb46fb3399a428dde4e44558c700c
Jason Chen <chen.hsinan2007@gmail.com>
2022-12-07 21:52:31 -0800

    update

---
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/In-place Reversal of a LinkedList/Reverse alternating K-element Sub-list (medium) v1.cpp
@@ -0,0 +1,36 @@
+/*
+
+https://www.educative.io/courses/grokking-the-coding-interview/m2YYJJRP9KG
+
+Reverse alternating K-element Sub-list (medium)
+Given the head of a LinkedList and a number ‘k’, reverse every alternating ‘k’ sized sub-list starting 
+from the head.
+
+If, in the end, you are left with a sub-list with less than ‘k’ elements, reverse it too.
+
+ Original List:  
+ Example:  
+ head  
+ head  
+    1   
+    2   
+    3   
+    4   
+    5   
+    6   
+    7   
+    8   
+    null   
+ Reversed Sub-list:  
+ k=2  
+    2   
+    1   
+    3   
+    4   
+    6   
+    5   
+    7   
+    8   
+
+*/
+
