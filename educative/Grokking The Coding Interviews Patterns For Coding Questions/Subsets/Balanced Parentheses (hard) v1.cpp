ac61129a1a1a0e9248b5747c74614c1a74180760
Jason Chen <chen.hsinan2007@gmail.com>
2022-12-10 19:48:45 -0800

    update

---
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/Subsets/Balanced Parentheses (hard) v1.cpp
@@ -0,0 +1,19 @@
+/*
+
+https://www.educative.io/courses/grokking-the-coding-interview/NEXBg8YA5A2
+
+Problem Statement
+For a given number ‘N’, write a function to generate all combination of ‘N’ 
+pairs of balanced parentheses.
+
+Example 1:
+
+Input: N=2
+Output: (()), ()()
+
+Example 2:
+
+Input: N=3
+Output: ((())), (()()), (())(), ()(()), ()()()
+
+*/
