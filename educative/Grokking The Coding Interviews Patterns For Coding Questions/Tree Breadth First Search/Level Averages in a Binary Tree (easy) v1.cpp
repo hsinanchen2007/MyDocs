2e5e3098a61fbe4071bfe209c5d48b5ca04513ad
Jason Chen <chen.hsinan2007@gmail.com>
2022-12-11 20:37:44 -0800

    update

---
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/Tree Breadth First Search/Level Averages in a Binary Tree (easy) v1.cpp
@@ -0,0 +1,31 @@
+/*
+
+https://www.educative.io/courses/grokking-the-coding-interview/YQWkA2l67GW
+
+Given a binary tree, populate an array to represent the averages of all of its levels.
+
+Example 1:
+
+    1   
+    2   
+    3   
+    4   
+    5   
+    6   
+    7   
+ [1, 2.5, 5.5]  
+ Level Averages:  
+
+Example 2:
+
+ Level Averages:  
+    12   
+    7   
+    1   
+    9   
+    2   
+    10   
+    5   
+ [12.0, 4.0, 6.5]  
+
+*/
