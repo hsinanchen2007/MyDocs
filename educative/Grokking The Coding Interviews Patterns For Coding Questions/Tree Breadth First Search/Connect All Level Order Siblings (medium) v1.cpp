b1a56282a1b2b75978841e8223c16cb4581bc304
Jason Chen <chen.hsinan2007@gmail.com>
2022-12-11 20:58:37 -0800

    update

---
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/Tree Breadth First Search/Connect All Level Order Siblings (medium) v1.cpp
@@ -0,0 +1,29 @@
+/*
+
+https://www.educative.io/courses/grokking-the-coding-interview/NE5109Jl02v
+
+Connect All Level Order Siblings (medium)
+
+Given a binary tree, connect each node with its level order successor. The last node of each level 
+should point to the first node of the next level.
+
+    1   
+    2   
+    3   
+    4   
+    5   
+    6   
+    7   
+ null  
+ Example 1:  
+    12   
+    7   
+    1   
+    9   
+    10   
+    5   
+ null  
+
+ Example 2:  
+
+*/
