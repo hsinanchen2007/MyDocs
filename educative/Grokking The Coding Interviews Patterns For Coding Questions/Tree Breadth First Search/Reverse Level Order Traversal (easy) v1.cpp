d3a0fad80a36683b7233d3c3f772f4fa72c063dc
Jason Chen <chen.hsinan2007@gmail.com>
2022-12-11 20:27:03 -0800

    update

---
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/Tree Breadth First Search/Reverse Level Order Traversal (easy) v1.cpp
@@ -0,0 +1,32 @@
+/*
+
+https://www.educative.io/courses/grokking-the-coding-interview/m2N6GwARL8r
+
+Given a binary tree, populate an array to represent its level-by-level traversal in reverse order, 
+i.e., the lowest level comes first. You should populate the values of all nodes in each level from 
+left to right in separate sub-arrays.
+
+Example 1:
+
+    1   
+    2   
+    3   
+    4   
+    5   
+    6   
+    7   
+ [[4,5,6,7],[2,3],[1]]  
+ Reverse Level Order Traversal:  
+
+Example 2:
+
+    12   
+    7   
+    1   
+    9   
+    10   
+    5   
+ [[9,10,5],[7,1],[12]]  
+ Reverse Level Order Traversal:  
+
+*/
