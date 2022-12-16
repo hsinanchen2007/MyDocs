503e08aaf1b8e4eddc1a7722b5f548f3286d64e9
Jason Chen <chen.hsinan2007@gmail.com>
2022-12-16 07:09:10 -0800

    update

---
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/Tree Depth First Search/Path with Maximum Sum (hard) v1.cpp
@@ -0,0 +1,37 @@
+/*
+
+https://www.educative.io/courses/grokking-the-coding-interview/B89q6NpX0Vx
+
+Find the path with the maximum sum in a given binary tree. Write a function that returns 
+the maximum sum.
+
+A path can be defined as a sequence of nodes between any two nodes and doesn’t necessarily 
+pass through the root. The path must contain at least one node.
+
+ Example 1:  
+    1   
+    2   
+    3   
+    4   
+    5   
+    6   
+
+ Output: 16
+ Explanation: The path with maximum sum is: [4, 2, 1, 3, 6]  
+ 
+ Example 2:  
+    1   
+    2   
+    3   
+    1   
+    3   
+    5   
+    6   
+    7   
+    8   
+    9
+
+ Output: 31
+ Explanation: The path with maximum sum is: [8, 5, 3, 6, 9]  
+
+*/
