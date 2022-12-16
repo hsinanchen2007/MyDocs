0f08a062ccdd76bede410ed94c368565015eddcb
Jason Chen <chen.hsinan2007@gmail.com>
2022-12-16 07:02:33 -0800

    update

---
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/Tree Depth First Search/Tree Diameter (medium) v1.cpp
@@ -0,0 +1,39 @@
+/*
+
+https://www.educative.io/courses/grokking-the-coding-interview/xVV1jA29YK9
+
+Tree Diameter (medium) 
+Given a binary tree, find the length of its diameter. The diameter of a tree is the number of 
+nodes on the longest path between any two leaf nodes. The diameter of a tree may or may not 
+pass through the root.
+
+Note: You can always assume that there are at least two leaf nodes in the given tree.
+
+Example 1:
+1
+2
+3
+4
+5
+6
+
+Output: 5
+Explanation: The diameter of the tree is: [4, 2, 1, 3, 6]
+
+Example 2:
+1
+2
+3
+5
+6
+7
+8
+10
+9
+11
+
+Output: 7
+Explanation: The diameter of the tree is: [10, 8, 5, 3, 6, 9, 11]
+
+*/
+
