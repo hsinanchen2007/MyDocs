627d399c44bca27a2246f442dec2f0d8da094bb3
Jason Chen <chen.hsinan2007@gmail.com>
2022-12-16 06:32:00 -0800

    update

---
+++ educative/Grokking The Coding Interviews Patterns For Coding Questions/Tree Depth First Search/Sum of Path Numbers (medium) v1.cpp
@@ -0,0 +1,29 @@
+/*
+
+https://www.educative.io/courses/grokking-the-coding-interview/YQ5o5vEXP69
+
+Problem Statement
+Given a binary tree where each node can only have a digit (0-9) value, each root-to-leaf path will 
+represent a number. Find the total sum of all the numbers represented by all paths.
+
+Example 1:
+1
+7
+9
+2
+9
+Output: 408 
+Explanation: The sum of all path numbers: 17 + 192 + 199
+
+Example 2:
+1
+0
+1
+1
+6
+5
+Output: 332 
+Explanation: The sum of all path numbers: 101 + 116 + 115
+
+*/
+
